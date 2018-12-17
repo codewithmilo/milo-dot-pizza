d578707d879e3f40fb38776b0a9373be8c448f35
Milo Watanabe <milo.wata@gmail.com>
2018-12-16 23:46:02 -0800

    get started on home page, and move some things around for future pages

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>milo.pizza</title>
+		<meta charset=UTF-8>
+		<link rel="stylesheet" type="text/css" href="/style/home.css" />
+		<script type="text/javascript" src="/js/home.js"></script>
+	</head>
+<body>
+
+<table>
+    <thead><td>MILO</td><td>DOT</td><td>PIZZA</td></thead>
+    <tr><td>hey</td></tr>
+</table>
+
+</body>
+</html>
---
+++ js/home.js
@@ -0,0 +1,14 @@
+window.onload = (function(){
+    var count = 1;
+    var firstColor = '0 0 10px #E9EDEF , 0 0 20px #E9EDEF , 0 0 30px #E9EDEF , 0 0 40px #AC10A8 , 0 0 70px #C210A1 , 0 0 80px #C210A1 , 0 0 100px #AC10A8';
+    var secondColor = '0 0 10px #E9EDEF , 0 0 20px #E9EDEF , 0 0 30px #E9EDEF , 0 0 40px #31A7D7 , 0 0 70px #31A7D7 , 0 0 80px #31A7D7 , 0 0 100px #31A7D7';
+    $tds = document.querySelectorAll('thead > tr > td');
+
+    setInterval(function(){
+        $tds.forEach($td => {
+            var colorToUse = (count % 2 == 0) ? secondColor : firstColor;
+            $td.style.textShadow = colorToUse;
+        });
+        count++;
+    }, 2000);
+});
