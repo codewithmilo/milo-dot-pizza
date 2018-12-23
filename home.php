56d46ea3b3a42ce6d90460630c41f67642236141
Milo Watanabe <milo.wata@gmail.com>
2018-12-23 12:05:42 -0800

    create a header and add a script. early doors for templating of sorts

--- home.php
+++ home.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>milo.pizza</title>
-		<meta charset=UTF-8>
 		<link rel="stylesheet" type="text/css" href="/style/home.css" />
-		<script type="text/javascript" src="/js/home.js"></script>
+		<script src="/js/header.js"></script>
 	</head>
 <body>
-
-<table>
-    <thead id="title"><td>MILO</td><td>DOT</td><td>PIZZA</td><td style="width: 100%"></td></thead>
-    <tr><td colspan="4">Hey, I'm Milo! I like trees, big cats, and the sun. I hope you enjoy my site, which is "Not Secure."</td></tr>
-    <tr><td colspan="4"></td></tr>
+<!-- The header creates an open table element, so we just add more rows and close it here -->
 </table>
 
 </body>
---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.write('\
+<!DOCTYPE html>\
+<html>\
+	<head>\
+		<title>milo.pizza</title>\
+		<meta charset=UTF-8>\
+	</head>\
+<body>\
+<!-- The header creates an open table element, so we just add more rows and close it here -->\
+<table>\
+    <thead id="title"><td>MILO</td><td>DOT</td><td>PIZZA</td><td style="width: 100%"></td></thead>\
+    <tr id="header"><td><b>About</b></td><td><b>Stuff</b></td><td><b>Links</b></td></tr>\
+');
