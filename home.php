3b665661caa6f4bdaaaed30c1ab280aab2e2aa02
Milo Watanabe <milo.wata@gmail.com>
2018-12-26 23:34:26 -0800

    add a counter

--- home.php
+++ home.php
@@ -9,5 +9,11 @@
 	<tr><td>Hi, I'm Milo. Welcome to my website!<br/>Please enjoy fun stuff I've made, cool links I've found, and interesting writing I've done.<br/>Thank you, enjoy!</td></tr>
 	<tr><td><img src="/images/car-still.png" /></td></tr>
 </table>
+<div id="counter">
+	<?php
+		$url = "//counter.websiteout.net/compte.php?S=".urlencode($_SERVER['HTTP_HOST'])."&C=2&D=0&N=0&M=0";
+		echo "<img src=$url alt='' border='0' />";
+	?>
+</div>
 </body>
 </html>
