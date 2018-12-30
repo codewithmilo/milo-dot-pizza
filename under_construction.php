1a9ede76fd6ce253bedda31eb7dfb10ec1d35829
Milo Watanabe <milo.wata@gmail.com>
2018-12-29 19:13:55 -0800

    let you go back

--- under_construction.php
+++ under_construction.php
@@ -12,10 +12,20 @@
                 margin: 8% auto;
                 width: 300px;
                 height: auto;
-			}
+            }
+            p {
+                text-align: center;
+                font-size: 24pt;
+                font-family: "Comic Sans MS", "Comic Sans", cursive;
+            }
+            p a {
+                color: black;
+                text-decoration: none;
+            }
 		</style>
 	</head>
 <body>
 <img src="./images/under-construction.gif" />
+<p><a href="home">Back</p>
 </body>
 </html>
