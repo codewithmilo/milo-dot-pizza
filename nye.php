9f1ee078b890c25793381e32e1be04fdd209c90a
Milo Watanabe <milo.wata@gmail.com>
2018-12-31 13:26:46 -0800

    new years eve countdown clock

---
+++ nye.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Countdown</title>
+		<meta charset=UTF-8>
+		<link rel="stylesheet" type="text/css" href="/style/nye.css" />
+		<script type="text/javascript" src="/js/nye.js"></script>
+	</head>
+<body>
+
+<div id="container">
+    <h1 id="clock"></h1>
+</div>
+
+<div id="fireworks">
+	<img id="f2" src="./images/fireworks2.gif" />
+	<img id="f3" src="./images/fireworks3.gif" />
+	<img id="f1" src="./images/fireworks1.gif" />
+</div>
+
+</body>
+</html>
