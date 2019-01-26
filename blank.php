583dfdc4fc2ef35b7f3ea22e5d52b3f17bb288cb
Milo Watanabe <milo.wata@gmail.com>
2019-01-26 12:41:23 -0800

    change the blog

---
+++ blank.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+            h1 {
+                width: 50%;
+                margin: 0 auto;
+            }
+        </style>
+	</head>
+<body>
+            <h1>This page has been left intentionally blank.</h1>
+</body>
+</html>
